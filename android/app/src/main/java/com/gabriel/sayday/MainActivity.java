0afd4b9316d013663ae2e726826ea6337a03bb9e
vodagabriel11 <voda_gabriel@ymail.com>
2026-03-12 23:37:29 +0200

    fix table.sql

---
+++ android/app/src/main/java/com/gabriel/sayday/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gabriel.sayday;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gabriel.sayday',
+  appName: 'Sayday',
+  webDir: 'dist/public'
+};
+
+export default config;
