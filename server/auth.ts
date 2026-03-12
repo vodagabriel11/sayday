71c09587ed1e69b40a7ff8729dde5740c29c2f0e
vodagabriel11 <voda_gabriel@ymail.com>
2026-03-12 23:52:48 +0200

    fix session cookie

--- server/auth.ts
+++ server/auth.ts
@@ -51,7 +51,7 @@ export function setupAuth(app: Express) {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: "none",
       },
     })
   );
